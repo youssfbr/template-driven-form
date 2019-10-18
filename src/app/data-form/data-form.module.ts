2a8026e0208b2176af2e208c96bbfdd07af969e6
Alisson Youssf <youssfbr@gmail.com>
2019-10-17 21:08:00 -0300

    DataFormsModule

---
+++ src/app/data-form/data-form.module.ts
@@ -0,0 +1,19 @@
+import { SharedModule } from './../shared/shared.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { DataFormComponent } from './data-form.component';
+
+
+@NgModule({
+  declarations: [DataFormComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    SharedModule,
+    HttpClientModule
+  ]
+})
+export class DataFormModule { }
